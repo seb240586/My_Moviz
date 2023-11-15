a86e9d98ddf1b369ae773abc8fecbe6cb9437888
sébastien <sebastien.faure24@orange.fr>
2023-11-15 15:49:56 +0100

    test

---
+++ backend/routes/index.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var router = express.Router();
+const api_key = process.env.OWM_API_KEY
+
+
+
+router.get('/movies', (req, res) => {
+    fetch(`https://api.themoviedb.org/3/discover/movie?api_key=${api_key}`)
+        .then(response => response.json())
+        .then(data => {
+            res.json({ movies: data.results });
+        });
+});
+
+module.exports = router;
